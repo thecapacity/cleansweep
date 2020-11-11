268d716c5ef3a3e364b191dff6a5a02ebfa5d4b9
jay <thecapacity@users.noreply.github.com>
2020-11-11 13:34:04 -0500

    Add hash function

--- db.py
+++ db.py
@@ -10,5 +10,16 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+def hash_func(file_name):
+    BLOCKSIZE = 65536
+    hasher = hashlib.sha1()
+
+    with open(file_name,'rb') as afile:
+        buf = afile.read(BLOCKSIZE)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = afile.read(BLOCKSIZE)
+        return hasher.hexdigest()
+
 def init_app(app):
 	pass
