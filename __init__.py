06c369eccd56330736cf68f6ce67bb42533bfaa8
jay <thecapacity@users.noreply.github.com>
2020-11-11 13:30:08 -0500

    Create __init__.py
    Initial skeleton

---
+++ __init__.py
@@ -0,0 +1,36 @@
+import os
+import hashlib
+import dataset
+import json
+
+from flask import Flask, url_for, render_template, request, redirect, g, current_app
+from markupsafe import escape
+
+def create_app(test_config=None):
+
+    app = Flask(__name__, instance_relative_config=True)
+
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/')
+    def index():
+        return 'Hello, World!'
+
+
+    return app
