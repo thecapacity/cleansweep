589403d4f2b331bf2b4ecbb6edadd42f6f92d4b2
jay <thecapacity@users.noreply.github.com>
2020-11-26 14:00:24 -0500

    Future Development Note

--- db.py
+++ db.py
@@ -42,6 +42,7 @@ def get_files(dir_name = None):
 
     return child_files, sub_dirs
 
+    ## FIXME: eventually consider yield vs. building a full list
     """for f in os.listdir(path):
         if check_file( os.path.join(path, f) ):
             yield f
